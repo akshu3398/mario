bbc2d95d160e2a1bbdd64c60d4f7b48a7a534cb1
3398abhinav <3398abhinav@gmail.com>
2020-01-27 19:51:29 +0530

    added collision detection

--- Player.lua
+++ Player.lua
@@ -1,139 +1,240 @@
-Player = Class()
+--[[
+    Represents our player in the game, with its own sprite.
+]]
 
-require("Animation")
+Player = Class{}
 
-local MOVE_SPEED = 80
+local WALKING_SPEED = 140
 local JUMP_VELOCITY = 400
-local GRAVITY = 40
 
-function Player:init( map )
+function Player:init(map)
+    
+    self.x = 0
+    self.y = 0
     self.width = 16
     self.height = 20
 
-    self.x = map.tileWidth * 10
-    self.y = map.tileHeight * (map.mapHeight / 2 - 1) - self.height
+    -- offset from top a to center to support sprite flipping
+    self.xOffset = 8
+    self.yOffset = 10
 
+    -- reference to map for checking tiles
+    self.map = map
+    self.texture = love.graphics.newImage('graphics/blue_alien.png')
+
+    -- animation frames
+    self.frames = {}
+
+    -- current animation frame
+    self.currentFrame = nil
+
+    -- used to determine behavior and animations
+    self.state = 'idle'
+
+    -- determines sprite flipping
+    self.direction = 'a'
+
+    -- x and y velocity
     self.dx = 0
     self.dy = 0
 
-    self.map = map
-    self.texture  = love.graphics.newImage('graphics/blue_alien.png')
-    self.frames = generateQuads(self.texture, self.width, self.height)
+    -- position on top of map tiles
+    self.y = map.tileHeight * ((map.mapHeight - 2) / 2) - self.height
+    self.x = map.tileWidth * 10
 
-    self.state = 'idle'
-    self.direction = 'right'
-
+    -- initialize all player animations
     self.animations = {
-        ['idle'] = Animation {
+        ['idle'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[1]
-            },
-            interval = 1
-        },
-        ['walking'] = Animation {
+                love.graphics.newQuad(0, 0, 16, 20, self.texture:getDimensions())
+            }
+        }),
+        ['walking'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[9], self.frames[10], self.frames[11]
+                love.graphics.newQuad(128, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(160, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
             },
             interval = 0.15
-        },
-        ['jumping'] = Animation {
+        }),
+        ['jumping'] = Animation({
             texture = self.texture,
             frames = {
-                self.frames[3]
-            },
-            interval = 1
-        },
+                love.graphics.newQuad(32, 0, 16, 20, self.texture:getDimensions())
+            }
+        })
     }
 
+    -- initialize animation and current frame we should render
     self.animation = self.animations['idle']
+    self.currentFrame = self.animation:getCurrentFrame()
 
+    -- behavior map we can call based on player state
     self.behaviors = {
-        ['idle'] = function ( dt )
+        ['idle'] = function(dt)
+            
+            -- add spacebar functionality to trigger jump state
             if love.keyboard.wasPressed('space') then
                 self.dy = -JUMP_VELOCITY
                 self.state = 'jumping'
                 self.animation = self.animations['jumping']
-            elseif love.keyboard.isDown('a')  then
-                self.dx = -MOVE_SPEED
+            elseif love.keyboard.isDown('a') then
+                self.direction = 'a'
+                self.dx = -WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
                 self.animation = self.animations['walking']
-                self.direction = 'left'
             elseif love.keyboard.isDown('d') then
-                self.dx = MOVE_SPEED
+                self.direction = 'd'
+                self.dx = WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
                 self.animation = self.animations['walking']
-                self.direction = 'right'
             else
                 self.dx = 0
-                self.animation = self.animations['idle']
             end
         end,
-        ['walking'] = function ( dt )
+        ['walking'] = function(dt)
+            
+            -- keep track of input to switch movement while walking, or reset
+            -- to idle if we're not moving
             if love.keyboard.wasPressed('space') then
                 self.dy = -JUMP_VELOCITY
                 self.state = 'jumping'
                 self.animation = self.animations['jumping']
-            elseif love.keyboard.isDown('a')  then
-                self.dx = -MOVE_SPEED
-                self.animation = self.animations['walking']
-                self.direction = 'left'
+            elseif love.keyboard.isDown('a') then
+                self.direction = 'a'
+                self.dx = -WALKING_SPEED
             elseif love.keyboard.isDown('d') then
-                self.dx = MOVE_SPEED
-                self.animation = self.animations['walking']
-                self.direction = 'right'
+                self.direction = 'd'
+                self.dx = WALKING_SPEED
             else
                 self.dx = 0
+                self.state = 'idle'
                 self.animation = self.animations['idle']
             end
-        end,
-        ['jumping'] = function ( dt )
-            if love.keyboard.isDown('a')  then
-                self.dx = -MOVE_SPEED
-                self.direction = 'left'
-            elseif love.keyboard.isDown('d') then
-                self.dx = MOVE_SPEED
-                self.direction = 'right'
-            end
-            self.dy = self.dy + GRAVITY
 
-            if self.y >= map.tileHeight * (map.mapHeight / 2 - 1) - self.height then
-                self.y = map.tileHeight * (map.mapHeight / 2 - 1) - self.height
+            -- check for collisions moving a and d
+            self:checkRightCollision()
+            self:checkLeftCollision()
+
+            -- check if there's a tile directly beneath us
+            if not self.map:collides(self.map:tileAt(self.x, self.y + self.height)) and
+                not self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
+                
+                -- if so, reset velocity and position and change state
+                self.state = 'jumping'
+                self.animation = self.animations['jumping']
+            end
+        end,
+        ['jumping'] = function(dt)
+            -- break if we go below the surface
+            if self.y > 300 then
+                return
+            end
+
+            if love.keyboard.isDown('a') then
+                self.direction = 'a'
+                self.dx = -WALKING_SPEED
+            elseif love.keyboard.isDown('d') then
+                self.direction = 'd'
+                self.dx = WALKING_SPEED
+            end
+
+            -- apply map's gravity before y velocity
+            self.dy = self.dy + self.map.gravity
+
+            -- check if there's a tile directly beneath us
+            if self.map:collides(self.map:tileAt(self.x, self.y + self.height)) or
+                self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
+                
+                -- if so, reset velocity and position and change state
                 self.dy = 0
                 self.state = 'idle'
-                self.animation = self.animations[self.state]
+                self.animation = self.animations['idle']
+                self.y = (self.map:tileAt(self.x, self.y + self.height).y - 1) * self.map.tileHeight - self.height
             end
-        end,
+
+            -- check for collisions moving a and d
+            self:checkRightCollision()
+            self:checkLeftCollision()
+        end
     }
 end
 
-function Player:update( dt )
+function Player:update(dt)
     self.behaviors[self.state](dt)
     self.animation:update(dt)
+    self.currentFrame = self.animation:getCurrentFrame()
     self.x = self.x + self.dx * dt
-    self.y = self.y + self.dy * dt
 
+    -- if we have negative y velocity (jumping), check if we collide
+    -- with any blocks above us
     if self.dy < 0 then
-        if self.map:tileAt(self.x, self.y) ~= TILE_EMPTY or self.map:tileAt(self.x + self.width - 1, self.y) ~= TILE_EMPTY then
+        if self.map:tileAt(self.x, self.y).id ~= TILE_EMPTY or
+            self.map:tileAt(self.x + self.width - 1, self.y).id ~= TILE_EMPTY then
+            -- reset y velocity
             self.dy = 0
 
-            if self.map:tileAt(self.x, self.y) == JUMP_BLOCK then
-                self.map:setTile(math.floor(self.x / self.map.tileWidth) + 1, 
-                    math.floor( self.y / self.map.tileHeight ) + 1, JUMP_BLOCK_HIT)
+            -- change block to different block
+            if self.map:tileAt(self.x, self.y).id == JUMP_BLOCK then
+                self.map:setTile(math.floor(self.x / self.map.tileWidth) + 1,
+                    math.floor(self.y / self.map.tileHeight) + 1, JUMP_BLOCK_HIT)
             end
-
-            if self.map:tileAt(self.x + self.width - 1, self.y) == JUMP_BLOCK then
-                self.map:setTile(math.floor((self.x + self.width - 1) / self.map.tileWidth) + 1, 
-                    math.floor( self.y / self.map.tileHeight ) + 1, JUMP_BLOCK_HIT)
+            if self.map:tileAt(self.x + self.width - 1, self.y).id == JUMP_BLOCK then
+                self.map:setTile(math.floor((self.x + self.width - 1) / self.map.tileWidth) + 1,
+                    math.floor(self.y / self.map.tileHeight) + 1, JUMP_BLOCK_HIT)
             end
+        end
+    end
 
+    -- apply velocity
+    self.y = self.y + self.dy * dt
+end
+
+-- checks two tiles to our a to see if a collision occurred
+function Player:checkLeftCollision()
+    if self.dx < 0 then
+        -- check if there's a tile directly beneath us
+        if self.map:collides(self.map:tileAt(self.x - 1, self.y)) or
+            self.map:collides(self.map:tileAt(self.x - 1, self.y + self.height - 1)) then
+            
+            -- if so, reset velocity and position and change state
+            self.dx = 0
+            self.x = self.map:tileAt(self.x - 1, self.y).x * self.map.tileWidth
         end
     end
 end
 
-function Player:render( )
-    local scaleX = self.direction == 'right' and 1 or -1
-    love.graphics.draw(self.texture, self.animation:getCurrentFrame(), 
-        math.floor(self.x + self.width / 2), math.floor(self.y + self.height / 2),
-        0, scaleX, 1,
-        self.width / 2, self.height / 2)
+-- checks two tiles to our d to see if a collision occurred
+function Player:checkRightCollision()
+    if self.dx > 0 then
+        -- check if there's a tile directly beneath us
+        if self.map:collides(self.map:tileAt(self.x + self.width, self.y)) or
+            self.map:collides(self.map:tileAt(self.x + self.width, self.y + self.height - 1)) then
+            
+            -- if so, reset velocity and position and change state
+            self.dx = 0
+            self.x = (self.map:tileAt(self.x + self.width, self.y).x - 1) * self.map.tileWidth - self.width
+        end
+    end
+end
+
+function Player:render()
+    local scaleX
+
+    -- set negative x scale factor if facing a, which will flip the sprite
+    -- when applied
+    if self.direction == 'd' then
+        scaleX = 1
+    else
+        scaleX = -1
+    end
+
+    -- draw sprite with scale factor and offsets
+    love.graphics.draw(self.texture, self.currentFrame, math.floor(self.x + self.xOffset),
+        math.floor(self.y + self.yOffset), 0, scaleX, 1, self.xOffset, self.yOffset)
 end
--- main.lua
+++ main.lua
@@ -1,60 +1,109 @@
-WINDOW_WIDTH = 1280*3/4
-WINDOW_HEIGHT = 720*3/4
+--[[
+    Super Mario Bros. Demo
+    Author: Colton Ogden
+    Original Credit: Nintendo
 
+    Demonstrates rendering a screen of tiles.
+]]
+
+Class = require 'class'
+push = require 'push'
+
+require 'Animation'
+require 'Map'
+require 'Player'
+
+-- close resolution to NES but 16:9
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-Class = require("class")
-push = require("push")
+-- actual window resolution
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-require("Util")
-require("Map")
+-- seed RNG
+math.randomseed(os.time())
 
-function love.load(  )
+-- an object to contain our map data
+map = Map()
 
-    math.randomseed(os.time())
-
-    map = Map()
-
+-- performs initialization of all objects and data needed by program
+function love.load()
+    
+    -- makes upscaling look pixel-y instead of blurry
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- sets up a different, better-looking retro font as our default
     love.graphics.setFont(love.graphics.newFont('fonts/font.ttf', 8))
 
+    -- sets up virtual screen resolution for an authentic retro feel
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = true,
-        vsync = true
+        resizable = true
     })
 
     love.keyboard.keysPressed = {}
+    love.keyboard.keysReleased = {}
 end
 
-function love.resize( w, h )
+-- called whenever window is resized
+function love.resize(w, h)
     push:resize(w, h)
 end
 
-function love.keypressed( key )
+-- global key pressed function
+function love.keyboard.wasPressed(key)
+    if (love.keyboard.keysPressed[key]) then
+        return true
+    else
+        return false
+    end
+end
+
+-- global key released function
+function love.keyboard.wasReleased(key)
+    if (love.keyboard.keysReleased[key]) then
+        return true
+    else
+        return false
+    end
+end
+
+-- called whenever a key is pressed
+function love.keypressed(key)
     if key == 'escape' then
-        -- the function LÖVE2D uses to quit the application
         love.event.quit()
     end
+
     love.keyboard.keysPressed[key] = true
 end
 
-function love.keyboard.wasPressed( key )
-    return love.keyboard.keysPressed[key]
+-- called whenever a key is released
+function love.keyreleased(key)
+    love.keyboard.keysReleased[key] = true
 end
 
-function love.update( dt )
+-- called every frame, with dt passed in as delta in time since last frame
+function love.update(dt)
     map:update(dt)
 
+    -- reset all keys pressed and released this frame
     love.keyboard.keysPressed = {}
+    love.keyboard.keysReleased = {}
 end
 
-function love.draw(  )
+-- called each frame, used to render to the screen
+function love.draw()
+    -- begin virtual resolution drawing
     push:apply('start')
+
+    -- clear screen using Mario background blue
     love.graphics.clear(108/255, 140/255, 1, 1)
-    love.graphics.translate(math.floor( -map.camX + 0.5 ), math.floor( -map.camY + 0.5))
+
+    -- renders our map object onto the screen
+    love.graphics.translate(math.floor(-map.camX + 0.5), math.floor(-map.camY + 0.5))
     map:render()
+
+    -- end virtual resolution
     push:apply('end')
 end
